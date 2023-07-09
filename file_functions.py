dcfbb7449c6370370aec1631ad686e265a672bd9
andytestt <ipushok19@gmail.com>
2023-07-09 21:35:56 +0200

    Додав обробку якщо маєм більше номерів телефонів та емайлів.

--- file_functions.py
+++ file_functions.py
@@ -13,11 +13,26 @@ def load_data_from_files():
     return addressbook, notes
 
 def save_addressbook_to_csv(file_path, addressbook):
+    # Знаходимо максимальну кількість номерів телефону та адрес електронної пошти серед усіх контактів
+    max_phone_count = max([len(record.phone) for record in addressbook])
+    max_email_count = max([len(record.email) for record in addressbook])
+
+    # Створюємо заголовки для номерів телефону та адрес електронної пошти
+    phone_headers = [f"Phone {i+1}" for i in range(max_phone_count)]
+    email_headers = [f"Email {i+1}" for i in range(max_email_count)]
+
     with open(file_path, "w", newline="") as csv_file:
         writer = csv.writer(csv_file)
-        writer.writerow(["Name", "Phone", "Email"])
+        headers = ["Name", "Birthday"] + phone_headers + email_headers
+        writer.writerow(headers)
+
         for record in addressbook:
-            writer.writerow([str(record.name), record.phone, record.email])
+            # Заповнюємо порожні місця в номерах телефону та адресах електронної пошти
+            phone_numbers = record.phone + [""] * (max_phone_count - len(record.phone))
+            email_addresses = record.email + [""] * (max_email_count - len(record.email))
+
+            writer.writerow([str(record.name), record.birthday] + phone_numbers + email_addresses)
+
     print("Address book saved to CSV:", file_path)
 
 def load_contacts_from_csv(file_path):
@@ -25,11 +40,15 @@ def load_contacts_from_csv(file_path):
     with open(file_path, "r") as csv_file:
         reader = csv.reader(csv_file)
         header = next(reader)
+        
+        phone_indices = [i for i, col in enumerate(header) if "Phone" in col]
+        email_indices = [i for i, col in enumerate(header) if "Email" in col]
+        
         for row in reader:
             name = Name(row[0])
             birthday = Birthday(row[1])
-            phone_numbers = [num for num in row[2:6] if num != "-"]
-            email_addresses = [email for email in row[6:8] if email != "-"]
+            phone_numbers = [row[i] for i in phone_indices if i < len(row) and row[i] != ""]
+            email_addresses = [row[i] for i in email_indices if i < len(row) and row[i] != ""]
             addressbook.add_record(Record(name, phone=phone_numbers, email=email_addresses, birthday=birthday))
     return addressbook
 
